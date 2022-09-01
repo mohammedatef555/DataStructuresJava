655a673ce9a425add479cac38938ae8997a1a530
mohammedatef555 <42421703+mohammedatef555@users.noreply.github.com>
2022-09-01 21:18:24 +0400

    Stack Array Based Implementation

---
+++ src/stackArrayBasedImp/Stack.java
@@ -0,0 +1,71 @@
+package stackArrayBasedImp;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class Stack<T> {
+    public T[] array;
+    private int top;
+    private int capacity;
+
+    public Stack(int capacity) {
+        this.capacity = capacity;
+        array = (T[])new Object[capacity];
+        top = 0;
+    }
+
+    public boolean isFull() {
+        return top == capacity;
+    }
+
+    public boolean isEmpty() {
+        return top == 0;
+    }
+
+    public T top() {
+        if (!isEmpty())
+            return array[top - 1];
+        throw new RuntimeException("Empty Stack");
+    }
+
+    public int size() {
+        return top;
+    }
+
+    public void clearStack() {
+        top = 0;
+    }
+
+    private void traverseStack(int index,Consumer<T> consumer) {
+        if (index < top - 1)
+            traverseStack(index + 1, consumer);
+        consumer.accept(array[index]);
+    }
+
+    public void traverseStack(Consumer<T> consumer) {
+        traverseStack(0, consumer);
+    }
+
+    public void push(T element) {
+        if (!isFull()) {
+            array[top++] = element;
+        } else {
+            System.out.println("Start extending the stack");
+            System.out.println("The capacity now is " + capacity);
+            capacity = array.length * 2;
+            array = Arrays.copyOf(array, array.length * 2);
+            System.out.println("The capacity now is " + capacity);
+            array[top++] = element;
+        }
+//        throw new RuntimeException("Full Stack");
+    }
+
+    public T pop() {
+        if (!isEmpty()) {
+            return array[--top];
+        }
+        throw new RuntimeException("Empty Stack");
+    }
+
+}
